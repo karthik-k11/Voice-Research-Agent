8958c96a414825d707456aa9985b6ba030bf6a32
Karthik <karthikk1162@gmail.com>
2025-12-21 15:52:51 +0530

    Wikipedia setup

--- app/agents/researcher.py
+++ app/agents/researcher.py
@@ -2,3 +2,12 @@
 import wikipedia
 import arxiv
 from duckduckgo_search import DDGS
+
+#Search Wikipedia function
+def search_wiki(query, sentences=3):
+    try:
+        print(f"🔎 Searching Wikipedia for: {query}")
+        summary = wikipedia.summary(query, sentences=sentences)
+        return summary
+    except Exception as e:
+        return f"Wikipedia error: {e}"
